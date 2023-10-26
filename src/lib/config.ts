e084b6d653d62371e0aafd3eef851e92ad24ada0
amarukawanobiru <k4tsu0502@gmail.com>
2023-10-26 19:01:40 +0900

    📝 Authorコンポーネントの作成

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -5,7 +5,37 @@ export interface Post {
   body: string;
 }
 
+interface GeoLocation {
+  lat: string;
+  lng: string;
+}
+
+interface Address {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: GeoLocation;
+}
+
+interface Company {
+  name: string;
+  catchPhrase: string;
+  bs: string;
+}
+
+export interface Author {
+  id: string;
+  name: string;
+  username: string;
+  email: string;
+  address: Address;
+  phone: string;
+  website: string;
+  company: Company;
+}
+
 export const endpoint = {
   posts: import.meta.env.PUBLIC_JSONPLACEHOLODER_POSTS_API,
-  users: import.meta.env.PUBLIC_JSONPLACEHOLODER_USERS_API,
+  authors: import.meta.env.PUBLIC_JSONPLACEHOLODER_USERS_API,
 };
